a5f07f7f1660ddf56bdc4df5ac63209af351ca02
raynaputera <raynaputera@gmail.com>
2021-10-18 13:57:00 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+class contoh01 extends CI_controller
+{
+public function index()
+{
+echo"(hl)perkenalan</hl>";
+echo"Nama saya RAYNA PUTERA DIIAH PAMBUDI
+    saya tinggal di daerah kertasari
+    olahraga yang saya sukai adalah
+    lari dan badminton no telp 081804707551":
+    }
+}
+?>   
